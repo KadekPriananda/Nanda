79b448a55976073e8d2808680942e3a7df9a6429
KadekPriananda <151504851+KadekPriananda@users.noreply.github.com>
2023-11-20 15:24:57 +0800

    Upload Tugas Pemrograman Web

---
+++ latihan.php
@@ -0,0 +1,10 @@
+<?php
+
+$server   = "localhost";
+$user     = "root";
+$password = "";
+$database  = "2101020042_ikadek_priananda";
+
+$con = mysqli_connect($server, $user, $password, $database);
+
+?>
